b6e0aa9a7174a3c3aeba688798b853c1d5c68d63
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2023-03-09 14:51:06 -0800

    Python - If else loops function created

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# 101-remove_char_at.py
-
-
 def remove_char_at(str, n):
-    """Create a copy of the string without the character at position n."""
-    if n < 0:
-        return (str)
+    new = ""
+    i = 0
+    for c in str:
+        if i != n:
+            new += c
+        i += 1
+    return new
