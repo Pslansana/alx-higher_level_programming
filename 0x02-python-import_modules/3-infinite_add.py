e9ee60c2db79266c4242bf1b2e08c22a8b52dcf8
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2023-03-10 17:07:48 +0000

    Python Import modules file created

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,11 +5,4 @@ if __name__ == "__main__":
     for arg in sys.argv:
         if arg != sys.argv[0]:
             sum += int(arg)
-    print(sum)#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    sum = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            sum += int(arg)
     print(sum)
