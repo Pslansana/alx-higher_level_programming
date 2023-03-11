cdda13de40beb1927eb72510f7207ac8f9bd9143
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2023-03-11 07:37:47 +0000

    Technical interview preparation:

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,141 +1,25 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stdlib.h>
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-listint_t *prev = NULL;
-listint_t *current = *head;
-listint_t *next = NULL;
-
-while (current)
-{
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
-}
-
-*head = prev;
-}
-
-/**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Return: 1 if it is, 0 if not
+ * Description: singly linked list node structure
+ * for project
  */
-int is_palindrome(listint_t **head)
+typedef struct listint_s
 {
-listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
-if (*head == NULL || (*head)->next == NULL)
-return (1);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-while (1)
-{
-fast = fast->next->next;
-if (!fast)
-{
-dup = slow->next;
-break;
-}
-if (!fast->next)
-{
-dup = slow->next->next;
-break;
-}
-slow = slow->next;
-}
+int is_palindrome(listint_t **head);
 
-reverse_listint(&dup);
-
-while (dup && temp)
-{
-if (temp->n == dup->n)
-{
-dup = dup->next;
-temp = temp->next;
-}
-else
-return (0);
-}
-
-if (!dup)
-return (1);
-
-return (0);
-}#include "lists.h"
-
-/**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-listint_t *prev = NULL;
-listint_t *current = *head;
-listint_t *next = NULL;
-
-while (current)
-{
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
-}
-
-*head = prev;
-}
-
-/**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
- *
- * Return: 1 if it is, 0 if not
- */
-int is_palindrome(listint_t **head)
-{
-listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
-
-if (*head == NULL || (*head)->next == NULL)
-return (1);
-
-while (1)
-{
-fast = fast->next->next;
-if (!fast)
-{
-dup = slow->next;
-break;
-}
-if (!fast->next)
-{
-dup = slow->next->next;
-break;
-}
-slow = slow->next;
-}
-
-reverse_listint(&dup);
-
-while (dup && temp)
-{
-if (temp->n == dup->n)
-{
-dup = dup->next;
-temp = temp->next;
-}
-else
-return (0);
-}
-
-if (!dup)
-return (1);
-
-return (0);
-}
+#endif /* LISTS_H */
