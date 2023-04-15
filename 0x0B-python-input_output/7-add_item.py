4e4629f5dc041e73fe84cbd4e135ee82d99e257f
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2023-04-15 00:40:51 +0000

    Write a script that adds all arguments to a Python list, and then save them to a file:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 """
-Module 7-add_item
-Contains function that adds and saves to Python obj to JSON file; loads objects
-# run with ./7-add_item.py
-#
-# cat add_item.json ; echo ""
-# expect output: []
-#
-# ./7-add_item.py some random args
-# cat add_item.json ; echo ""
-# expect output: ["some", "random", "args"]
+    Python script that adds all args to a Python List.
+    List is then saved to a file.
 """
-
 
 
 import sys
