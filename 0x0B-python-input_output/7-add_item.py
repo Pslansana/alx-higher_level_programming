5d3748e4f4154b7e82d9653e4b49475ad497c856
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-03-24 11:32:21 -0700

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-def add_to_list_and_save(arguments):
-    try:
-        # Load existing items from the file if it exists
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        # If the file doesn't exist, start with an empty list
-        items = []
-
-    # Add new arguments to the list
-    items.extend(arguments)
-
-    # Save the updated list to the file
-    save_to_json_file(items, "add_item.json")
 
 if __name__ == "__main__":
-    # Exclude the script name (sys.argv[0]) from the list of arguments
-    arguments = sys.argv[1:]
-    add_to_list_and_save(arguments)
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
